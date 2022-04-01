95b93ef3282c6caebf0f060815b58dfd9b353fb0
xyzw88 <xyzw88@gmail.com>
2022-04-01 12:02:21 +0800

    Create useragent.js

---
+++ useragent.js
@@ -0,0 +1,4 @@
+let headers = $request.headers;
+headers['User-Agent'] = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/99.0.4844.84 Safari/537.36';
+
+$done({headers});
