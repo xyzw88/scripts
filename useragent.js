20a8625b7886f2c3af768d93fbf1f39cb7f504fb
xyzw88 <xyzw88@gmail.com>
2022-04-02 17:16:50 +0800

    Update useragent.js

--- useragent.js
+++ useragent.js
@@ -1,4 +1,4 @@
-let headers = $request.headers;
-headers['User-Agent'] = 'Mozilla/5.0 (iPhone; CPU iPhone OS 15_4 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) COS/100.0.4896.56 Mobile/15E148 Safari/604.1';
+let body = $request.body;
+body = body.replace('/Android|webOS|iPhone|iPod|BlackBerry/i.test(navigator.userAgent)', '/Android|webOS|iPhone|iPad|iPod|BlackBerry/i.test(navigator.userAgent)')
 
-$done({headers});
+$done({body});
